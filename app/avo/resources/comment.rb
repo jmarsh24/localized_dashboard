cd786aee8c908a9eb4de90b281d1bc9052bfdfc6
Justin Marsh <jmarsh24@gmail.com>
2024-11-25 17:30:33 +0100

    show

--- app/avo/resources/comment.rb
+++ app/avo/resources/comment.rb
@@ -8,12 +8,11 @@ class Avo::Resources::Comment < Avo::BaseResource
   def fields
     field :body, as: :textarea, translation_key: "avo.field_translations.comment.body"
     field :commentable,
-    as: :belongs_to,
-    polymorphic_as: :commentable,
-    types: [ ::Post ],
-    polymorphic_help: "Choose the type of record",
-    help: "Choose the record you need.",
-    translation_key: "avo.field_translations.comment.commentable",
-    hide_on: :show
+      as: :belongs_to,
+      polymorphic_as: :commentable,
+      types: [ ::Post ],
+      polymorphic_help: "Choose the type of record",
+      help: "Choose the record you need.",
+      translation_key: "avo.field_translations.comment.commentable"
   end
 end
